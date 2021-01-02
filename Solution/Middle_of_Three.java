fa3ad03729a83b84378357c3d4eb95a507d097a9
Ayush Joshi <59890645+AyushJoshi2001@users.noreply.github.com>
2021-01-02 20:58:41 +0530

    Create Middle_of_Three.java

---
+++ Solution/Middle_of_Three.java
@@ -0,0 +1,20 @@
+class Solution{
+    int middle(int A, int B, int C)
+    {
+        //code here
+        if((A<B && B<C) || (C<B && B<A))
+        {
+            return B;
+        }
+        
+        else if((B<A && A<C) || (C<A && A<B))
+        {
+            return A;
+        }
+        
+        else
+        {
+            return C;
+        }
+    }
+}
